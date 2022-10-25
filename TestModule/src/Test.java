d2f7cd442f08fc071bc6a6616c2a31828214ee90
Wualter01 <kidink1993@gmail.com>
2022-10-25 14:19:44 -0400

    i added new java file

--- TestModule/src/Test.java
+++ TestModule/src/Test.java
@@ -3,4 +3,10 @@ public class Test {
         System.out.println("Hello world");
         System.out.println("finally got gitHub to work");
     }
+    /*what is git? what is github? what is the difference
+    git is a version control system that let you keep track of your source code history .
+    gitHub is a cloud based hosting service that lets you manage git repositories.
+
+    when we create our project from vcs we clone our project.
+     */
 }
